a11ba4839f95e6ab3e9dc2ddd8b64b076feaede2
jokreLan <jokreLan>
2018-05-17 16:12:36 +0800

    commmit demo3

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 这个demo是一个简单的webpack示例集.
 
 These demos are purposely written in a simple and clear style. You will find no difficulty in following them to learn the powerful tool.
+
---
+++ demo03/main.jsx
@@ -0,0 +1,17 @@
+// const React = require('react');
+// const ReactDOM = require('react-dom');
+
+// ReactDOM.render(
+//   <h1>Hello, world!</h1>,
+//   document.querySelector('#wrapper')
+// );
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>hello world demo03!</h1>,
+    document.querySelector("#wrapper")
+
+)
+
